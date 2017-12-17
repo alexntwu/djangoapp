17777501bfc7fda40313e9850e87077425a1965e
alexntwu <alexnt.wu@gmail.com>
2017-12-17 20:56:13 +0800

    finish https://openhome.cc/Gossip/CodeData/PythonTutorial/FormCSRF.html Practice 13

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
-from polls.models import Poll
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from polls.models import Poll, Choice
 
 
 def index(request):
@@ -20,9 +20,22 @@ def detail(request, poll_id):
     return render(request, 'polls/detail.html', {'poll': poll})
 
 
+#def results(request, poll_id):
+#    return HttpResponse("You're looking at the results of poll {id}.".format(id = poll_id))
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll {id}.".format(id = poll_id))
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll': poll})
 
 
+#def vote(request, poll_id):
+#    return HttpResponse("You're voting on poll {id}.".format(id = poll_id))
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll {id}.".format(id = poll_id))
+    p=get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'poll': p, 'error_message':'did not make selection'})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
